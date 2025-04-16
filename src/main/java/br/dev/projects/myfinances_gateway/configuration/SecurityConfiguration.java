a38571beb50e02449a9f52e73a40d96416048f72
Éverton <dev@evert90.com>
2025-04-16 06:29:48 -0300

    fix: pwa

--- src/main/java/br/dev/projects/myfinances_gateway/configuration/SecurityConfiguration.java
+++ src/main/java/br/dev/projects/myfinances_gateway/configuration/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
                         .csrfTokenRequestHandler(requestHandler)
                 )
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/api/**", "/version/**", "/gateway/version/**")
+                        .pathMatchers("/manifest.json", "/api/**", "/version/**", "/gateway/version/**")
                         .permitAll()
                         .anyExchange()
                         .authenticated()
