fcc52569b687ada644356ef7443b448689e490be
Éverton <dev@evert90.com>
2025-04-18 17:45:17 -0300

    feat: decrease session timeout + sentry

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,10 @@ FROM eclipse-temurin:21-jre
 WORKDIR /app
 COPY --from=build /app/build/libs/*.jar app.jar
 
+COPY sentry/sentry-opentelemetry-agent-8.8.0.jar /sentry/sentry-opentelemetry-agent.jar
+
+ENV SENTRY_AUTO_INIT false
+
 EXPOSE 8080
 
-CMD ["java","-Djava.security.egd=file:/dev/./urandom","-jar","app.jar"]
+CMD ["java", "-Djava.security.egd=file:/dev/./urandom", "-javaagent:/sentry/sentry-opentelemetry-agent.jar", "-jar", "app.jar"]
--- src/main/java/br/dev/projects/myfinances_gateway/service/VersionService.java
+++ src/main/java/br/dev/projects/myfinances_gateway/service/VersionService.java
@@ -2,7 +2,8 @@ package br.dev.projects.myfinances_gateway.service;
 
 import br.dev.projects.myfinances_gateway.bean.Version;
 import br.dev.projects.myfinances_gateway.exception.VersionNotFoundException;
-import org.springframework.boot.info.GitProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import reactor.core.publisher.Mono;
@@ -12,21 +13,24 @@ import static reactor.core.publisher.Mono.just;
 @Service
 public class VersionService {
 
+    private final BuildProperties buildProperties;
+
     public static final String NOT_FOUND_MESSAGE = "Versão não encontrada";
 
-    private final GitProperties gitProperties;
+    @Value("${SOURCE_COMMIT:0.0.0}")
+    private String version;
 
-    public VersionService(GitProperties gitProperties) {
-        this.gitProperties = gitProperties;
+    public VersionService(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
     }
 
     public Mono<Version> getVersion() {
-        return just(new Version(gitProperties.getCommitId(), gitProperties.getCommitTime()));
+        return just(new Version(version, buildProperties.getTime()));
     }
 
     public Mono<Version> getVersionByGitVersion(@PathVariable(value = "gitVersion") String gitVersion) {
-        if(gitProperties.getCommitId().equals(gitVersion)) {
-            return just(new Version(gitProperties.getCommitId(), gitProperties.getCommitTime()));
+        if(version.equals(gitVersion)) {
+            return just(new Version(version, buildProperties.getTime()));
         }
 
         throw new VersionNotFoundException(NOT_FOUND_MESSAGE);
